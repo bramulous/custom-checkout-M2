d9d8be2ed7486d12b203ee45e71a17fcfa800a39
bramulous <>
2018-05-29 14:20:53 +0200

    Created module files

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_CheckoutAgreements/js/model/agreements-assigner':'BRVM_CustomCheckout/js/model/agreements-assigner'
+        }
+    }
+};
